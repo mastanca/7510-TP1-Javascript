551719d872d4b8c86ad19180c1ac7c02fd17a89f
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2017-10-05 16:25:48 -0300

    Remove TODOs

--- test/interpreterAcceptanceTests.js
+++ test/interpreterAcceptanceTests.js
@@ -67,8 +67,6 @@ describe("Interpreter", function () {
             assert(interpreter.checkQuery('padre(mario, pepe)') === false);
         });
 
-        // TODO: Add more tests
-
     });
 
     describe('Interpreter Rules', function () {
@@ -84,8 +82,6 @@ describe("Interpreter", function () {
         it('hijo(pepe, juan) should be true', function () {
             assert(interpreter.checkQuery('hijo(pepe, juan)'));
         });
-
-        // TODO: Add more tests
 
     });
 
